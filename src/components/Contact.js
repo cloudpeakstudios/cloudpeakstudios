f0df526a01ae684ac8ebb73ca217c978f308203a
Chowdhurian <manil.chowdhury@gmail.com>
2018-02-23 15:48:43 -0800

    add link to schedule a callback

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Contact = (props) => (
     <section id="contact">
         <div className="inner">
-            <section>
+            <section  >
                 <form method="post" action="#">
                     <div className="field half first">
                         <label htmlFor="name">Name</label>
@@ -29,6 +29,11 @@ const Contact = (props) => (
                         <span className="icon alt fa-envelope"></span>
                         <h3>Email</h3>
                         <a href="#">getstarted@cloudpeak.co</a>
+                    </div>
+                    <div className="contact-method">
+                        <span className="icon alt fa-phone"></span>
+                        <h3>Phone</h3>
+                        <a href="https://calendly.com/cloudpeak-studios">schedule a callback</a>
                     </div>
                 </section>
             </section>
